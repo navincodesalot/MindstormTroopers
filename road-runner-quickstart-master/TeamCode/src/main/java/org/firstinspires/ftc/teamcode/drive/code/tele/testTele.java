8822e071a4d634c35f7c7093f2b6a39258c87f28
Navin Narayanan <nan.navinnarayanan@gmail.com>
2023-02-11 00:45:58 -0600

    remove fsm

--- road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/code/tele/testTele.java
+++ road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/code/tele/testTele.java
@@ -25,26 +25,6 @@ import org.firstinspires.ftc.teamcode.drive.code.util.pidf.slidePIDF;
 
 @TeleOp(name = "W TELE")
 public class testTele extends LinearOpMode {
-    enum Mode {
-        DRIVER_CONTROL,
-        CYCLE
-    }
-
-    enum AutoMode {
-        INIT,
-        GRAB_CONE,
-        PICK_CONE,
-        LIFT_ARM,
-        INTO_BUCKET,
-        BACK_DOWN_ARM,
-        SLIDE_UP,
-        DROP_CONE,
-        RESET,
-        RESET_SLIDE
-    }
-
-    Mode currentMode = Mode.DRIVER_CONTROL;
-    AutoMode autoCurrentMode;
 
     public DcMotorEx arm;
     public DcMotorEx slide;
@@ -100,9 +80,6 @@ public class testTele extends LinearOpMode {
             telemetry.addData("hz ", 1000000000 / (loop - loopTime));
             loopTime = loop;
             telemetry.update();
-
-            switch (currentMode) {
-                case DRIVER_CONTROL:
                     drive.setWeightedDrivePower(
                             new Pose2d(
                                     -gamepad1.left_stick_y * 0.60,
@@ -118,10 +95,10 @@ public class testTele extends LinearOpMode {
                     if (gamepad1.a) { //pickup
                         bclaw.setPosition(0);
                     }
-                    if (gamepad2.right_bumper) {
+                    if (gamepad1.right_bumper) {
                         slideTarget = 2350;
                     }
-                    if (gamepad2.left_bumper) {
+                    if (gamepad1.left_bumper) {
                         slideTarget = 100;
                     }
                     if (gamepad2.x) { //close claw
@@ -139,93 +116,6 @@ public class testTele extends LinearOpMode {
                     if (gamepad2.y) {
                         armTarget = armDrop;
                     }
-                    if (gamepad1.x) {
-                        currentMode = Mode.CYCLE;
-                        autoCurrentMode = AutoMode.INIT;
-                    }
-                    break;
-                case CYCLE:
-                        telemetry.addData("Saving", targetPos);
-//                        targetPos = slide.getCurrentPosition();
-                    targetPos = 2350;
-
-                    switch (autoCurrentMode) {
-                        case INIT:
-                            if (gamepad1.b && targetPos != 0) {
-                                arm.setPower(armPIDF.returnPower(arm.getCurrentPosition(), armTarget));
-                                autoCurrentMode = AutoMode.GRAB_CONE;
-                            }
-                            break;
-                        case GRAB_CONE:
-                                armTarget = armPickup; //grab cone
-                                claw.setPosition(1); //open claw
-                                autoCurrentMode = AutoMode.PICK_CONE;
-                            slide.setPower(slidePIDF.returnPower(slide.getCurrentPosition(), slideTarget));
-                            arm.setPower(armPIDF.returnPower(arm.getCurrentPosition(), armTarget));
-                                break;
-                        case PICK_CONE:
-                            if (Math.abs(arm.getCurrentPosition() - armTarget) < armTresh) {
-                                claw.setPosition(clawClose); //close claw
-                                autoCurrentMode = AutoMode.LIFT_ARM;
-                            }
-                            slide.setPower(slidePIDF.returnPower(slide.getCurrentPosition(), slideTarget));
-                            arm.setPower(armPIDF.returnPower(arm.getCurrentPosition(), armTarget));
-                            break;
-                        case LIFT_ARM:
-                                armTarget = armDrop;
-                            arm.setPower(armPIDF.returnPower(arm.getCurrentPosition(), armTarget));
-                            autoCurrentMode = AutoMode.INTO_BUCKET;
-                                break;
-                        case INTO_BUCKET:
-                            if (Math.abs(arm.getCurrentPosition() - armTarget) < armTresh) {
-                                claw.setPosition(1); //open claw
-                                autoCurrentMode = AutoMode.BACK_DOWN_ARM;
-                                arm.setPower(armPIDF.returnPower(arm.getCurrentPosition(), armTarget));
-                            }
-                            slide.setPower(slidePIDF.returnPower(slide.getCurrentPosition(), slideTarget));
-                            break;
-                        case BACK_DOWN_ARM:
-                                armTarget = armPickup; //grab cone
-                            arm.setPower(armPIDF.returnPower(arm.getCurrentPosition(), armTarget));
-                            autoCurrentMode = AutoMode.SLIDE_UP;
-                                break;
-
-                        case SLIDE_UP:
-                            if (Math.abs(arm.getCurrentPosition() - armTarget) < armTresh) {
-                                slideTarget = targetPos;
-                                slide.setPower(slidePIDF.returnPower(slide.getCurrentPosition(), slideTarget));
-                                claw.setPosition(clawClose); //close claw
-                                autoCurrentMode = AutoMode.DROP_CONE;
-                            }
-                            break;
-                        case DROP_CONE:
-                            if ((Math.abs(slide.getCurrentPosition() - slideTarget) < slideTresh)) {
-                                bclaw.setPosition(0.92);
-                                ElapsedTime bclawTimer = new ElapsedTime();
-                                btime = 1; // 1 second wait
-                                if (bclawTimer.seconds() >= btime) {
-                                    bclawTimer.reset();
-                                    autoCurrentMode = AutoMode.RESET;
-                                }
-                            }
-                            break;
-                        case RESET:
-                            if (Math.abs(arm.getCurrentPosition() - armTarget) < armTresh) {
-                                bclaw.setPosition(0); //reset
-                                autoCurrentMode = AutoMode.RESET_SLIDE;
-                            }
-                            break;
-                        case RESET_SLIDE:
-                            slideTarget = 0; // todo: look into combining the reset slide and reset bucket for more time
-                            slide.setPower(slidePIDF.returnPower(slide.getCurrentPosition(), slideTarget));
-                            autoCurrentMode = AutoMode.LIFT_ARM;
-                            break;
-                    }
-                if (gamepad1.right_bumper) {
-                    currentMode = Mode.DRIVER_CONTROL;
-                    break;
-                }
-            }
         }
     }
 }
