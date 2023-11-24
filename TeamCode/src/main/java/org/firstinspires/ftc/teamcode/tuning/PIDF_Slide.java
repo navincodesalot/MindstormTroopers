6d8c44e3e67b0cffec7fd03830bf650523771b34
Navin Narayanan <nan.navinnarayanan@gmail.com>
2023-11-24 16:43:19 -0600

    tune slide

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/PIDF_Slide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/PIDF_Slide.java
@@ -4,17 +4,20 @@ import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.arcrobotics.ftclib.controller.PIDController;
+import com.arcrobotics.ftclib.controller.PIDFController;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @Config
 @TeleOp
 public class PIDF_Slide extends OpMode {
     private PIDController controller;
 
-    public static double p = 0, i = 0, d = 0;
-    public static double f = 0;
+    public static double p = 0.003, i = 0.25, d = 0.001;
+    public static double f = 0.0105;
 
     public static int target = 0;
 
@@ -28,6 +31,17 @@ public class PIDF_Slide extends OpMode {
 
         rightSlide = hardwareMap.get(DcMotorEx.class, "rightSlide");
         leftSlide = hardwareMap.get(DcMotorEx.class, "leftSlide");
+        leftSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftSlide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        leftSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        rightSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightSlide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        rightSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        rightSlide.setDirection(DcMotorEx.Direction.REVERSE);
     }
 
     @Override
@@ -47,7 +61,7 @@ public class PIDF_Slide extends OpMode {
     public double returnPower(int pos, int target) {
         controller.setPID(p, i, d);
         double pid = controller.calculate(pos, target);
-        double ff = target * f;
+        double ff = f;
 
         double power = pid + ff;
 
