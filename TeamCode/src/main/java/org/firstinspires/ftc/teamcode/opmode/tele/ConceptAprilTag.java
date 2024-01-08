80a45b713d78d0b078ab75174d97eafe783ec5bc
Navin Narayanan <nan.navinnarayanan@gmail.com>
2024-01-08 16:27:36 -0600

    test atag in tele

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/tele/ConceptAprilTag.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/tele/ConceptAprilTag.java
@@ -27,13 +27,20 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.firstinspires.ftc.robotcontroller.external.samples;
+package org.firstinspires.ftc.teamcode.opmode.tele;
 
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.IMU;
+
 import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.teamcode.roadrunner.drive.DriveConstants;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
@@ -69,6 +76,9 @@ import java.util.List;
 public class ConceptAprilTag extends LinearOpMode {
 
     private static final boolean USE_WEBCAM = true;  // true for webcam, false for phone camera
+    private IMU imu;
+    private final Vector2d camera1Offset = new Vector2d(0, 0); // todo: find in inches
+    private final Vector2d camera2Offset = new Vector2d(0, -6.375);
 
     /**
      * The variable to store our instance of the AprilTag processor.
@@ -84,6 +94,13 @@ public class ConceptAprilTag extends LinearOpMode {
     public void runOpMode() {
 
         initAprilTag();
+
+        imu = hardwareMap.get(IMU.class, "imu"); // init imu first for teleop (takes longer to init)
+        imu.initialize(new IMU.Parameters(
+                new RevHubOrientationOnRobot(
+                        DriveConstants.LOGO_FACING_DIR, DriveConstants.USB_FACING_DIR
+                )
+        ));
 
         // Wait for the DS start button to be touched.
         telemetry.addData("DS preview on/off", "3 dots, Camera Stream");
@@ -154,7 +171,7 @@ public class ConceptAprilTag extends LinearOpMode {
 
         // Set the camera (webcam vs. built-in RC phone camera).
         if (USE_WEBCAM) {
-            builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
+            builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 2"));
         } else {
             builder.setCamera(BuiltinCameraDirection.BACK);
         }
@@ -184,13 +201,42 @@ public class ConceptAprilTag extends LinearOpMode {
 
     }   // end method initAprilTag()
 
+    public Vector2d getFCPosition(AprilTagDetection detection, double botheading) {
+        Vector2d cameraOffset = camera2Offset;
+        // get coordinates of the robot in RC coordinates
+        // ensure offsets are RC
+        double x = detection.ftcPose.x - cameraOffset.getX();
+        double y = detection.ftcPose.y - cameraOffset.getY();
+
+        // invert heading to correct properly
+        botheading = -botheading;
+
+        // rotate RC coordinates to be field-centric
+        double x2 = x * Math.cos(botheading) + y * Math.sin(botheading);
+        double y2 = x * -Math.sin(botheading) + y * Math.cos(botheading);
+        double absX;
+        double absY;
+
+        // add FC coordinates to apriltag position
+        VectorF tagpose = detection.metadata.fieldPosition;
+        if (detection.metadata.id <= 6) { // first 6 are backdrop tags
+            absX = tagpose.get(0) + y2;
+            absY = tagpose.get(1) - x2;
+
+        } else { // then just reverse positions
+            absX = tagpose.get(0) - y2;
+            absY = tagpose.get(1) + x2;
+        }
+        // Don't send over a pose, as apriltag heading can be off (see discord)
+        return new Vector2d(absX, absY);
+    }
 
     /**
      * Add telemetry about AprilTag detections.
      */
     private void telemetryAprilTag() {
-
         List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+
         telemetry.addData("# AprilTags Detected", currentDetections.size());
 
         // Step through the list of detections and display info for each one.
@@ -200,6 +246,8 @@ public class ConceptAprilTag extends LinearOpMode {
                 telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
                 telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
                 telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
+                telemetry.addData("April Tag Pose", getFCPosition(currentDetections.get(currentDetections.size() - 1), imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES)).toString());
+                telemetry.addData("Heading", imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES));
             } else {
                 telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
                 telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
