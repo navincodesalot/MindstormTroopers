0ac68c1bcfc829a73e61a43ec97f524c38fecd6e
Navin Narayanan <nan.navinnarayanan@gmail.com>
2024-01-14 09:54:47 -0600

    fix meepmeep constraints

--- MeepMeep/src/main/java/com/example/meepmeep/Constraints.java
+++ MeepMeep/src/main/java/com/example/meepmeep/Constraints.java
@@ -1,9 +1,9 @@
 package com.example.meepmeep;
 
 public class Constraints {
-    public static double TRACK_WIDTH = 11.694; // in
+    public static double TRACK_WIDTH = 11.359; // in
     public static double MAX_VEL = 35;
     public static double MAX_ACCEL = 35;
-    public static double MAX_ANG_VEL = Math.toRadians(180);
-    public static double MAX_ANG_ACCEL = Math.toRadians(180);
+    public static double MAX_ANG_VEL = Math.toRadians(200);
+    public static double MAX_ANG_ACCEL = Math.toRadians(200);
 }
